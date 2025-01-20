eace87870ce5ad6c93463be22930d38a0316ead3
Aaron <caper2711@hotmail.com>
2025-01-19 21:45:41 -0600

    Benchmark padding Comparison of appending zeros manually through for loop against Sprintf, difference of nanoseconds but hey, something is something, even for only two characters

---
+++ base64/benchPadding_test.go
@@ -0,0 +1,71 @@
+package base64
+
+import (
+	"fmt"
+	"testing"
+)
+
+var result string
+
+// More optimal even tough makes one more allocation than format
+func manualPad6(str string) string {
+	strLen := len(str)
+	finalStr := str
+	for strLen != 6 {
+		finalStr = "0" + finalStr
+		strLen++
+	}
+	return finalStr
+}
+
+func formatPad6(str string) string {
+	return fmt.Sprintf("%06s", str)
+}
+
+func BenchmarkManualPad6(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s = manualPad6("123")
+	}
+	result = s
+}
+
+func BenchmarkFormatPad6(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s = formatPad6("123")
+	}
+	result = s
+}
+
+
+func manualPad8(str string) string {
+	strLen := len(str)
+	finalStr := str
+	for strLen != 8 {
+		finalStr = "0" + finalStr
+		strLen++
+	}
+	return finalStr
+}
+
+// More optimal in every aspect (memory, allocations and swiftness)
+func formatPad8(str string) string {
+	return fmt.Sprintf("%08s", str)
+}
+
+func BenchmarkManualPad8(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s = manualPad8("123")
+	}
+	result = s
+}
+
+func BenchmarkFormatPad8(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		s = formatPad8("123")
+	}
+	result = s
+}
